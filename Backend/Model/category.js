ffe5fe7f1c839a9d8bd3c74ed4d34599ef2732f5
salmaelbehairy261 <105329494+salmaelbehairy261@users.noreply.github.com>
2023-02-16 00:51:13 +0200

    init

--- Backend/Model/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const CategorySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  icon: { type: String },
-});
-
-mongoose.model("category", CategorySchema);
--- Backend/Model/orderItem.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderItemSchema = new mongoose.Schema({
-  quantity: { type: Number, required: true },
-  product: { type: mongoose.Schema.Types.ObjectId, ref: "product" },
-});
-
-mongoose.model("orderItem", orderItemSchema);
