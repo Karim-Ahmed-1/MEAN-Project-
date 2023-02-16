87fa61261701be436dfdf6cb7cb7765a9376db46
salmaelbehairy261 <105329494+salmaelbehairy261@users.noreply.github.com>
2023-02-16 20:31:06 +0200

    components

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { HomeComponent } from './components/home/home.component';
 import { CartComponent } from './components/cart/cart.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsCategoriesComponent } from './components/products-categories/products-categories.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { CartComponent } from './components/cart/cart.component';
     NavbarComponent,
     SliderComponent,
     HomeComponent,
-    CartComponent
+    CartComponent,
+    FooterComponent,
+    ProductsCategoriesComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule
