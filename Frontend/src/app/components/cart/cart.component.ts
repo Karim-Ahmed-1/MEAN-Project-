15585bab8b9071a296591d1c3fac66b55aed2f9b
salmaelbehairy261 <105329494+salmaelbehairy261@users.noreply.github.com>
2023-02-16 22:40:21 +0200

    CartComponent

--- Frontend/src/app/components/cart/cart.component.ts
+++ Frontend/src/app/components/cart/cart.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
+items:{src:String,title:String,description:String,quantity:Number,price:Number}[]=[
+  {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:1,price:300},
+  {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:2,price:150},
+  {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:3,price:200}
+]
 
 }
