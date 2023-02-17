e7b9f40c4f155eb03d9ecad54bc8a5186a91043b
salmaelbehairy261 <105329494+salmaelbehairy261@users.noreply.github.com>
2023-02-17 03:53:21 +0200

    Edit Home Component

--- Frontend/src/app/components/cart/cart.component.ts
+++ Frontend/src/app/components/cart/cart.component.ts
@@ -11,5 +11,12 @@ items:{src:String,title:String,description:String,quantity:Number,price:Number}[
   {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:2,price:150},
   {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:3,price:200}
 ]
+total:Number=0
+calTotal(){
+  this.items.forEach(element => {
+    console.log(typeof(element.price))
+    //this.total+=element.price
+  });
+}
 
 }
--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -14,10 +14,10 @@ export class HomeComponent {
     {src:'assets/2.png',name:'White Top',section:'Casual-Woman',price:'$124',disc:'$170'},
     {src:'assets/3.png',name:'Cashmere Tank +Bag ',section:'lather - Bags ',price:'$12',disc:'$170'}
   ]
-  likes:{src:String,name:String,section:String}[]=[
-    {src:"assets/4.png",name:'Cashmere Tank +Bag ',section:'lather - Bags '},
-    {src:"assets/4.png",name:'Cashmere Tank +Bag ',section:'lather - Bags '},
-    {src:"assets/4.png",name:'Cashmere Tank +Bag ',section:'lather - Bags '},
-    {src:"assets/4.png",name:'Cashmere Tank +Bag ',section:'lather - Bags '}
+  categories:{src:String,name:String}[]=[
+    {src:"assets/4.png",name:'Bags '},
+    {src:"assets/4.png",name:'Clothes '},
+    {src:"assets/4.png",name:'Shoes '},
+    {src:"assets/4.png",name:'Accessories '}
   ]
 }
