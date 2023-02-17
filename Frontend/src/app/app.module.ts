09ab530c0d1a0985dbf73daa72ead1bf72d1f836
karim ahmed <ka1658115@gmail.com>
2023-02-17 15:46:22 +0200

    registration design completed

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -36,6 +36,7 @@ const routes:Routes=[
     CartComponent,
     FooterComponent,
     LoginPageComponent,
+    RegisterPageComponent,
     ProductsCategoriesComponent,
     ProductDetailsComponent,
     ProductComponent,
--- Frontend/src/app/components/register-page/register-page.component.ts
+++ Frontend/src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register-page',
@@ -6,5 +8,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent {
+  userForm=new FormGroup({
+    fullname:new FormControl('',[Validators.required]),
+    email:new FormControl('',[Validators.required,Validators.email]),
+    city:new FormControl('',[Validators.required]),
+    street:new FormControl('',[Validators.required]),
+    username:new FormControl('',[Validators.required]),
+    phone:new FormControl('',[Validators.required]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8)]),
 
+  });
+  get getfullname(){
+    return this.userForm.controls["fullname"];
+  }
+  get getemail(){
+    return this.userForm.controls["email"];
+  }
+  get getecity(){
+    return this.userForm.controls["city"];
+  }
+  get getstreet(){
+    return this.userForm.controls["street"];
+  }
+  get getusername(){
+    return this.userForm.controls["username"];
+  } get getphone(){
+    return this.userForm.controls["phone"];
+  }
+  get getpassword(){
+    return this.userForm.controls["password"];
+  }
+  login(e:any){
+    e.preventDefault();
+    //console.log(this.userForm)
+    if(this.userForm.status=="VALID")
+      console.log(this.userForm.value)
+  }
 }
