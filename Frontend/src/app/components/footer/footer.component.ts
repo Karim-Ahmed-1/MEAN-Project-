a40c03d67c37de14f1c9a0e10d7ea23d62c85b6e
karim ahmed <ka1658115@gmail.com>
2023-02-17 17:17:37 +0200

    added go to top button in footer component

--- Frontend/src/app/components/footer/footer.component.ts
+++ Frontend/src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { debounceTime, fromEvent, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  showBtn$ = fromEvent(document, 'scroll').pipe(
+    debounceTime(50),
+    map(() => window.scrollY > 500),
+    tap(() => console.log('sas'))
+  );
+
+  // not Cross browsing (works on chrome - firefox)
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
 
 }
