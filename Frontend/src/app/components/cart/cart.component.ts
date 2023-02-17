7e50e146537e0bf085a8e61707d01933a0304f8e
salmaelbehairy261 <105329494+salmaelbehairy261@users.noreply.github.com>
2023-02-17 04:26:12 +0200

    Edit Cart

--- Frontend/src/app/components/cart/cart.component.ts
+++ Frontend/src/app/components/cart/cart.component.ts
@@ -6,17 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
-items:{src:String,title:String,description:String,quantity:Number,price:Number}[]=[
-  {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:1,price:300},
-  {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:2,price:150},
-  {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:3,price:200}
-]
-total:Number=0
-calTotal(){
-  this.items.forEach(element => {
-    console.log(typeof(element.price))
-    //this.total+=element.price
-  });
-}
-
+  items:{src:String,title:String,description:String,quantity:number,price:any}[]=[
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:1,price:300},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:1,price:150},
+    {src:"https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-shopping-carts/img6.webp",title:"Shirt",description:"Cotton T-shirt",quantity:1,price:200}
+  ]
+  total:any;
+  quantityElement:any;
+  calTotal(){
+    this.total=0
+    this.items.forEach(element => {
+      this.total+=element.price*element.quantity
+    });
+  }
+  constructor(){
+    this.calTotal();
+  }
+  increaseQuantity(e:any,i:any){
+    this.quantityElement=e.target.parentNode.parentNode.querySelector('input[type=number]')
+    this.quantityElement.stepUp()
+    this.items[i].quantity=this.quantityElement.value
+    this.calTotal()
+  }
+  decreaseQuantity(e:any,i:any){
+    this.quantityElement=e.target.parentNode.parentNode.querySelector('input[type=number]')
+    this.quantityElement.stepDown()
+    this.items[i].quantity=this.quantityElement.value
+    this.calTotal()
+  }
+  removeItem(index:any){
+    this.items=this.items.filter((item,i)=>index!=i)
+    this.calTotal()
+  }
 }
